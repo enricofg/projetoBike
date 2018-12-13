7f834a6af81cd7dc8ab56c74f06762577b5335c3
Ana Cassia <anacassia.10@hotmail.com>
2018-12-13 13:04:03 +0000

    Add files via upload
    Dia 1

---
+++ ProjetoDayy1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "funcoes_bicicletas.h"
+#include "estruturas.h"
+#include "constantes.h"
+
+int main()
+{
+    tipoBike bicicleta={1,2,3,4}
+}
