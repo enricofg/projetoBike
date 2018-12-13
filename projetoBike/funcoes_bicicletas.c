653ffe09987c6f5bfee924e92f27594072ae5edf
Ana Cassia <anacassia.10@hotmail.com>
2018-12-13 13:34:00 +0000

    Base do projeto feita em aula

---
+++ projetoBike/funcoes_bicicletas.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "funcoes_bicicletas.h"
+#include "estruturas.h"
+#include "constantes.h"
---
+++ projetoBike/funcoes_bicicletas.h
@@ -0,0 +1,7 @@
+#ifndef FUNCOES_BICICLETAS_H_INCLUDED
+#define FUNCOES_BICICLETAS_H_INCLUDED
+
+#include "estruturas.h"
+#include "constantes.h"
+
+#endif // FUNCOES_BICICLETAS_H_INCLUDED
